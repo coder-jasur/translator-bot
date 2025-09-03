b3ab75344a98824c63586a3482fc693aa3343ce1
Jasur <jasurhikmatilloyev08@gmail.com>
2025-09-03 19:35:26 +0500

    edit admin menu

--- src/app/dialogs/admin/dialog.py
+++ src/app/dialogs/admin/dialog.py
@@ -7,13 +7,36 @@ from src.app.dialogs.admin.handlers import users_count_getter
 from src.app.states.admin import AdminStateSG
 from src.app.states.broadcast import BroadcasterSG
 
+# admin_menu = Dialog(
+#     Window(
+#         Const("Выберите действие"),
+#         Button(
+#             Const("подписки на канал "),
+#             id="subscription_channel"
+#         ),
+#         Start(
+#             Const("Рассылка"),
+#             id="broadcaster",
+#             state=BroadcasterSG.get_creative
+#         ),
+#         SwitchTo(
+#             Const("количества пользывателей"),
+#             id="users_count",
+#             state=AdminStateSG.users_count
+#         ),
+#         state=AdminStateSG.menu
+#     ),
+#     Window(
+#         Format("{users_count}"),
+#         SwitchTo(Const("назад"), id="back_to_admin_menu", state=AdminStateSG.menu),
+#         state=AdminStateSG.users_count,
+#         getter=users_count_getter
+#     ),
+# )
+
 admin_menu = Dialog(
     Window(
         Const("Выберите действие"),
-        Button(
-            Const("подписки на канал "),
-            id="subscription_channel"
-        ),
         Start(
             Const("Рассылка"),
             id="broadcaster",
