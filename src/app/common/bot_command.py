4bb880decd04bb7943552213fd9da40593d56c01
Jasur <jasurhikmatilloyev08@gmail.com>
2025-09-04 16:50:47 +0500

    bot command

--- src/app/common/bot_command.py
+++ src/app/common/bot_command.py
@@ -7,21 +7,19 @@ from src.app.core.config import Settings
 async def bot_commands(bot: Bot, settings: Settings):
     await bot.set_my_commands(
         commands=[
-            BotCommand(command="/start", description="Restart"),
-            BotCommand(command="/language", description="Choose a language"),
-            BotCommand(command="/translate", description="Translate text")
-        ],
+            BotCommand(command="start", description="Restart"),
+            BotCommand(command="language", description="Choose a language"),
+            BotCommand(command="translate", description="Translate text"),
+        ]
     )
-
     for admin_id in settings.admins_ids:
-        scoupe = BotCommandScopeChat(chat_id=int(admin_id))
-
+        scope = BotCommandScopeChat(chat_id=int(admin_id))
         await bot.set_my_commands(
             commands=[
-                BotCommand(command="/start", description="Restart"),
-                BotCommand(command="/language", description="Choose a language"),
-                BotCommand(command="/translate", description="Translate text"),
-                BotCommand(command="/admin_menu", description="Admin menu")
+                BotCommand(command="start", description="Restart"),
+                BotCommand(command="language", description="Choose a language"),
+                BotCommand(command="translate", description="Translate text"),
+                BotCommand(command="admin_menu", description="Admin menu"),
             ],
-            scope=scoupe
+            scope=scope
         )
