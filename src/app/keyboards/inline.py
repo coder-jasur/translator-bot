29d2e131fe10e8dfd2a7d5574a489abbe1f721bf
Jasur <jasurhikmatilloyev08@gmail.com>
2025-09-05 23:41:48 +0500

    edit button

--- src/app/keyboards/inline.py
+++ src/app/keyboards/inline.py
@@ -3,7 +3,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 back_to_admin_menu_keyboards = InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            InlineKeyboardButton(text="◀️ orqaga", callback_data="back_to_admin_menu")
+            InlineKeyboardButton(text="◀️ Назад", callback_data="back_to_admin_menu")
         ]
     ]
 )
